aeb4811294035cde3c8f619de8a5477b34873861
Federico De Santi <f.desanti@campus.unimib.it>
2025-09-22 11:54:03 +0200

    scaling

--- working/L07_numba_multiprocessing/Q3_scaling.py
+++ working/L07_numba_multiprocessing/Q3_scaling.py
@@ -2,37 +2,46 @@ import os
 import numpy as np
 os.environ['MKL_NUM_THREADS']      = '1'
 os.environ['OPENBLAS_NUM_THREADS'] = '1'
+np.seterr(divide='ignore', invalid='ignore')
+
+import pycbc.psd
+import pycbc.noise
+import multiprocess as mp
+import matplotlib.pyplot as plt
 
 from tqdm import tqdm
+from time import time
 from pycbc.waveform import get_td_waveform
+from pycbc.detector import Detector
 
-def generate_waveforms(N=10, m_min=100, m_max=1000, sampling_frequency=4096, duration=3600):   
-    M = np.random.uniform(m_min, m_max, N)
-    q = np.random.uniform(0.125, 1, N)
+L1 = Detector("L1")
 
-    m1 = M/(1+q)
-    m2 = q * m1
+def run(M, q, ra, dec, pol, psd, duration=128, sampling_frequency=4096):   
+    mass1 = M/(1+q)
+    mass2 = q * mass1
 
-    td_templates = []
-    for mass1, mass2 in tqdm(zip(m1, m2), total=N):
-        h, _ = get_td_waveform(approximant="IMRPhenomXPHM", mass1=mass1, mass2=mass2, f_lower=2, delta_t=1/sampling_frequency)
-        
-        if h.duration < duration:
-            n = int((duration - h.duration) * sampling_frequency)
-            h.prepend_zeros(n)
-        elif h.duration > duration:
-            diff = h.duration - duration
-            h.crop(left=diff, right=0)
-        td_templates.append(h)
-    return np.array(td_templates)
+    hplus, hcross = get_td_waveform(approximant="IMRPhenomXPHM", mass1=mass1, mass2=mass2, f_lower=2, delta_t=1/sampling_frequency)
+        # Adjust the length of the waveform to be exactly 'duration' seconds
+    if hplus.duration < duration:
+        n = int((duration - h.duration) * sampling_frequency)
+        hplus.prepend_zeros(n)
+        hcross.prepend_zeros(n)
+    elif hplus.duration > duration:
+        diff = hplus.duration - duration
+        hplus.crop(left=diff, right=0)
+        hcross.crop(left=diff, right=0)
+    
+    h = L1.project_wave(hplus, hcross, ra, dec, pol)
 
+    snr = optimal_snr(h, psd, sampling_frequency)
+    
+    return snr
 
 def noise_weighted_inner_product(a, b, psd, duration):
     """
     Computes the noise weighte inner product of two frequency domain signals a and b.
     
     Args:
-    -----
         a   (numpy.ndarray): frequency domain signal
         b   (numpy.ndarray): frequency domain signal
         psd (numpy.ndarray): power spectral density
@@ -40,7 +49,7 @@ def noise_weighted_inner_product(a, b, psd, duration):
     
     """
     integrand = np.conj(a) * b / psd
-    return (4 / duration) * np.sum(integrand, dim = -1)
+    return (4 / duration) * np.sum(integrand, -1)
 
 
 def optimal_snr(td_template, psd, sampling_frequency):
@@ -50,7 +59,7 @@ def optimal_snr(td_template, psd, sampling_frequency):
     (https://git.ligo.org/lscsoft/bilby/-/blob/master/bilby/gw/utils.py?ref_type=heads)
     
     Args:
-    -----
+
         td_domain_template (numpy.ndarray): time domain signal
         psd                (numpy.ndarray): power spectral density
         sampling_frequency         (float): sampling frequency of the signal
@@ -65,6 +74,50 @@ def optimal_snr(td_template, psd, sampling_frequency):
 
 
 
-if __name__=="__main__":    
-    h = generate_waveforms()
 
+
+
+if __name__=="__main__":    
+
+    # set seed
+    np.random.seed(42)
+
+    N = 50
+    M = np.random.uniform(100, 1000, N)
+    q = np.random.uniform(0.125, 1, N)
+    ra  = np.random.uniform(0, 2*np.pi, N)
+    dec = np.arcsin(np.random.uniform(-1, 1, N))
+    pol = np.random.uniform(0, np.pi, N)
+
+    # The color of the noise matches a PSD which you provide
+    flow = 2
+    delta_f = 1.0 / 128
+    flen = int(4096 / delta_f) + 1
+    psd = pycbc.psd.aLIGOZeroDetHighPower(flen, delta_f, flow)
+
+    # Generate 32 seconds of noise at 4096 Hz
+    delta_t = 1.0 / 4096
+    tsamples = int(128 / delta_t)
+    noise = pycbc.noise.noise_from_psd(tsamples, delta_t, psd, seed=42)
+
+    run_times = []
+    cpus_test = range(1, mp.cpu_count()+5)
+    for cpus in tqdm(cpus_test):
+        print(f"Running with {cpus} CPUs")
+        
+
+        with mp.Pool(processes=cpus) as pool:
+            start = time()
+            results = list(pool.imap(lambda x: run(x[0], x[1], x[2], x[3], x[4], x[5]), 
+                                        zip(M, q, ra, dec, pol, psd)))
+            end = time()
+            run_times.append(end - start)
+        
+    plt.figure()
+    plt.plot(cpus_test, run_times, marker='o')
+    plt.xlabel("Number of CPUs")
+    plt.ylabel("Run time [s]")
+    plt.axvline(os.cpu_count(), color='k', ls='--', label='Number of physical cores')
+    plt.title("Scaling of waveform generation with number of CPUs")
+    plt.legend()
+    plt.savefig("scaling.png", dpi=200, bbox_inches='tight')
